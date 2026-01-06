70cd89a71c7849582d9900070da1c2f72306f090
azzhftr <azzhfitria9@gmail.com>
2026-01-06 16:57:49 +0700

    Initial commit: Setup Laravel project with authentication and API

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl">
+            Tambah Produk
+        </h2>
+    </x-slot>
+
+    <div class="p-6">
+        <p>Halaman tambah produk (admin)</p>
+    </div>
+</x-app-layout>
